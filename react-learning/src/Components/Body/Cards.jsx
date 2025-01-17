08022eef96c3b9961db6ec0126e4a9e12240e89d
vivekanand <102859238+vnray@users.noreply.github.com>
2025-01-17 02:39:52 +0000

    card with data

--- react-learning/src/Components/Body/Cards.jsx
+++ react-learning/src/Components/Body/Cards.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Cards = () => {
+const [restrauntList, setRestrauntList] = useState([])
+  useEffect(()=>{
+    callApi();
+  },[])
+
   async function callApi() {
+  
     const res = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4694432&lng=77.0325445&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const data = await res.json();
-    const rData =
-      data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants[0]
-        .info;
+    
+     setRestrauntList( data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
   }
-  callApi();
 
-  return <h1> card section updated with live</h1>;
+
+  return (
+    <div className="cards">
+
+      {restrauntList.map((restraunt)=>{
+        return(
+<div className="card" key={restraunt.info.id}>
+        <img src={restraunt.info.cloudinaryImageId} alt="" />
+        <h3>{restraunt.info.name}</h3>
+        <h3>{restraunt.info.avgRating}</h3>
+        <h4>{restraunt.info.cuisines.join(", ")}</h4>
+        <h4>{restraunt.info.locality}</h4>
+      </div>
+        )
+      })}
+      
+    </div>
+  );
 };
 
 export default Cards;
