fd99548bb2b0576957a87e4a9499cff7927f85b5
vnray <ervnray@gmail.com>
2025-01-20 23:46:12 +0530

    header

--- react-learning/src/Components/Header/Header.jsx
+++ react-learning/src/Components/Header/Header.jsx
@@ -1,29 +1,5 @@
-import {Link} from "react-router-dom"
-// import {logo} from "./assets/myImages.jpg"
-// import {logo} from "../assets/myImages.jpg"
 const Header = () => {
-  return (
-    <header>
-   <div className="container">
-        <div className="logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <nav>
-          <ul>
-            {/* <Link to={"/"}><li>Home</li></Link>
-            <Link to={"/about"}><li>About</li></Link>
-            <Link to={"/contact"}><li>Contact</li></Link> */}
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <div className="user">
-          <button>Login</button>
-        </div>
-        </div>
-    </header>
-  );
+  return <h1>header section</h1>;
 };
 
 export default Header;
